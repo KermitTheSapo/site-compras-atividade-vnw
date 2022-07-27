29e14d6d0735157cf6d071bc59b01522d625318c
KermitTheSapo <rjsf06@gmail.com>
2022-07-27 13:55:29 -0300

    inicio do projeto

---
+++ script.js
@@ -0,0 +1,10 @@
+// let imagem = document.querySelector(".imagem")
+
+
+// function muda(imagem) {
+//     imagem.classList.remove("imagem")
+//     imagem.classList.add(".imagem2")
+// }
+
+// imagem.setAttribute("src", muda)
+
