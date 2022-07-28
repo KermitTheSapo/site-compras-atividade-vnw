25b0b282da102525d514c9cf36c9e2493b8763bf
KermitTheSapo <rjsf06@gmail.com>
2022-07-28 17:30:33 -0300

    adicionando js contador

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-// let imagem = document.querySelector(".imagem")
+let imagem = document.querySelector(".imagem")
+function muda() {
+    if (imagem.src="assets/cosiness.png") {
+        imagem.src="assets/intensity.png"
+    } else {
+        imagem.src="assets/cosiness.png"
+    }
+}
 
 
-// function muda(imagem) {
-//     imagem.classList.remove("imagem")
-//     imagem.classList.add(".imagem2")
+let numero = document.querySelector(".numero")
+let contador = 1;
+
+function Soma() {
+    numero.innerHTML = contador++
+}
+
+function Subtracao(){
+    if(contador > 0) {
+        numero.innerHTML = contador--
+    }
+}
+
+
+// function soma(){
+    
 // }
 
-// imagem.setAttribute("src", muda)
+// function diminuir(){
 
+// }
