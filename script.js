0d30e2d312f82fff7dc8f8439aa05850f1c881aa
KermitTheSapo <rjsf06@gmail.com>
2022-07-29 13:23:32 -0300

    contador funcionando

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-let imagem = document.querySelector(".imagem")
+const imagem = document.querySelector('.imagem')
+const titulo = document.querySelector('.titulo')
+let contador = '0'
+
 function muda() {
-    if (imagem.src="assets/cosiness.png") {
-        imagem.src="assets/intensity.png"
-    } else {
-        imagem.src="assets/cosiness.png"
-    }
+	if (contador === '0') {
+		imagem.src="assets/intensity.png"
+		contador = '1'
+        
+	} else {
+		imagem.src="assets/cosiness.png"
+		contador = '0'
+	}
 }
+
+
 
 
 let numero = document.querySelector(".numero")
-let contador = 1;
+let batata = 0;
 
 function Soma() {
-    numero.innerHTML = contador++
+    numero.innerHTML = batata+=1
 }
 
 function Subtracao(){
-    if(contador > 0) {
-        numero.innerHTML = contador--
+    if (batata > 0) {
+        numero.innerHTML = batata-=1
     }
 }
-
-
-// function soma(){
-    
-// }
-
-// function diminuir(){
-
-// }
